b600f7679b77fc1a6c0d6e136cff3e5b53962b6e
Nikolay Boev <nboev@bul-si.bg>
2025-07-26 00:24:59 +0300

    a

--- rent-fe/src/app/admin/rental-item/services/rental-item.service.ts
+++ rent-fe/src/app/admin/rental-item/services/rental-item.service.ts
@@ -18,7 +18,7 @@ export class RentalItemService {
       "Accept-Language": localStorage.getItem("lang") || "bg",
     });
     return this.http.get<RentalItem[]>(
-      `${environment.apiUrl}/api/v0.0.1/rental-items`,
+      `${environment.apiUrl}/api/v0.0.1/auth/rental-items`,
       { headers },
     );
   }
